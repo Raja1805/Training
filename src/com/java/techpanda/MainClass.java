7ccf6712d4ad675801777211d0f84f9fb080350a
deepika <deepikasenthil60@gmai.com>
2026-02-26 11:34:46 +0530

    new methods

--- src/com/java/techpanda/MainClass.java
+++ src/com/java/techpanda/MainClass.java
@@ -1,4 +1,19 @@
 package com.java.techpanda;
 
 public class MainClass {
+    public static void main(String[] args) {
+        System.out.println(sum(4, 8));
+        System.out.print(multi());
+    }
+
+    public static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static int multi() {
+        int num1 = 3;
+        int num2 = 5;
+        return num1 * num2;
+    }
 }
+
